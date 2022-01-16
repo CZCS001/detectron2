a3c272ed1b513a022cbc9a157502b3c8a582cc78
CZCS001 <87878099+CZCS001@users.noreply.github.com>
2022-01-16 13:08:21 -0500

    Update coco_evaluation.py

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -239,7 +239,8 @@ class COCOEvaluator(DatasetEvaluator):
         if self._output_dir:
             file_path = os.path.join(self._output_dir, "coco_instances_results.json")
             self._logger.info("~~COCOE~~~Saving results to {}".format(file_path))
-            self._logger.info("~~COCOE~~~Saving results to {}".format(str(file_path))
+            self._logger.info("~~COCOE~~~Saving results to {}".format(str(coco_results))
+            self._logger.info("~~COCOE~~~Saving results to {}".format(coco_results)
             with PathManager.open(file_path, "w") as f:
                 f.write(json.dumps(coco_results))
                 f.flush()
