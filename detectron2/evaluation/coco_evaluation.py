b297bf10052612763d2629aeae065c5fad29b054
CZCS001 <87878099+CZCS001@users.noreply.github.com>
2022-01-17 15:12:55 -0500

    Update coco_evaluation.py

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -640,10 +640,11 @@ def _evaluate_predictions_on_coco(
       print("stats:",stats)
       #names = {k: v for k, v in enumerate(["fuwo", "cewo", "zhanli"])}
      # stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
-      if len(stats) and stats[0].any():
-         p, r, ap, f1, ap_class = ap_per_class(*stats, plot=True, save_dir='./output/', names=names)
+      #if len(stats) and stats[0].any():
+      #   p, r, ap, f1, ap_class = ap_per_class(*stats, plot=True, save_dir='./output/', names=names)
      # C_M.plot(save_dir='./output/confusion_matrix_rec.png',names=["fuwo","cewo","zhanli"], rec_or_pred=0)
       C_M.plot(save_dir='./output/confusion_matrix_rec.png',names=["ballon"], rec_or_pred=0)
+      C_M.plot(save_dir='./output/confusion_matrix_pred.png',names=["ballon"], rec_or_pred=1)
      #C_M.plot(save_dir='./output/confusion_matrix_pred.png',names=["fuwo", "cewo", "zhanli"], rec_or_pred=1) 
    
 
