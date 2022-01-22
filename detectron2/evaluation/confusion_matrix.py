216a6eaeb58e80927b68088bff6545d47f65224f
CZCS001 <87878099+CZCS001@users.noreply.github.com>
2022-01-22 16:01:25 -0500

    Update confusion_matrix.py

--- detectron2/evaluation/confusion_matrix.py
+++ detectron2/evaluation/confusion_matrix.py
@@ -33,7 +33,9 @@ class ConfusionMatrix:
 
         detections = detections[detections[:, 4] > self.conf]
         gt_classes = labels[:, 0].int()
+        print("!!!!!!gt_classes!!!!!:",detection_classes)
         detection_classes = detections[:, 5].int()
+        print("!!!!!!detection_classes!!!!!:",detection_classes)
         iou = box_iou(labels[:, 1:], detections[:, :4])
 
         x = torch.where(iou > self.iou_thres)
