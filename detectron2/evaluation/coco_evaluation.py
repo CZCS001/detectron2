062601525a2750f5d50ec26dc23dc047c6c33437
CZCS001 <87878099+CZCS001@users.noreply.github.com>
2022-01-21 19:24:09 -0500

    Update coco_evaluation.py

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -608,33 +608,22 @@ def _evaluate_predictions_on_coco(
       for i in range(len(coco_gt.imgs)):# images
           bbox_gt = np.array([y['bbox'] for y in coco_gt.imgToAnns[i]])
           class_gt = np.array([[y['category_id']] for y in coco_gt.imgToAnns[i]]).  #2.coco_gt转化为labels
-          labels = np.hstack((class_gt,bbox_gt))
-            
+          labels = np.hstack((class_gt,bbox_gt)) 
           coco_gt_classes.append(class_gt)
-         # print("coco_gt.imgs",coco_gt.imgs)
-         # print("class_gt", class_gt)
           print("coco_gt.imgToAnns_ALL",coco_gt.imgToAnns)
-         # print("coco_gt.imgToAnns",coco_gt.imgToAnns[i])
-         # print("bbox_gt",bbox_gt)
-         # print("labels", labels)
-          
-            
-            
-            
-            (i)        
+          print("coco_gt.imgToAnns",coco_gt.imgToAnns[i])
+          print(i)        
           bbox_dt = np.array([y['bbox'] for y in coco_dt.imgToAnns[i]])    #1.coco_dt转化成predictions
          # print("bbox_dt:",bbox_dt)
           conf_dt = np.array([[y['score']] for y in coco_dt.imgToAnns[i]])
          # print("conf_dt:",conf_dt)
           class_dt = np.array([[y['category_id']] for y in coco_dt.imgToAnns[i]])
           print("coco_dt.imgToAnns_ALL",coco_dt.imgToAnns)
+          print("coco_gt.imgToAnns",coco_gt.imgToAnns[i])
           coco_gt_classes.append(class_dt)
-        #  print("class_dt:",class_dt)
           predictions = np.hstack((np.hstack((bbox_dt,conf_dt)),class_dt))
-         # print("predictions:",predictions)
           C_M.process_batch(predictions, labels)
           detects = torch.tensor(xywh2xyxy(predictions))
-        #  print("detects:",detects)
           labs = torch.tensor(np.hstack((labels[:, 0][:, None], xywh2xyxy(labels[:, 1:]))))
           iouv = torch.linspace(0.5, 0.95, 10)  # iou vector for mAP@0.5:0.95
           correct = process_batch(detects, labs, iouv)
